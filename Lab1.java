523d72bbc568132045c25090cc35097a148b1aff
Massih <massih.z@gmail.com>
2015-02-02 15:12:45 +0100

    thread initialization

--- Lab1.java
+++ Lab1.java
@@ -3,14 +3,18 @@ import java.util.concurrent.Semaphore;
 
 public class Lab1 {
 	static Semaphore sem1;
-	
-	
+	final int default_sim_speed = 100;
+	final int t1Speed=0;
+	final int t2Speed=0;
 	public static void main(String[] args) {
 		sem1 = new Semaphore(1);
 		new Lab1(args);
 	}
 
 	public Lab1(String[] args) {
+		if(args.length > 0){
+			
+		}
 		TSimInterface tsi = TSimInterface.getInstance();
 
 		try {
@@ -20,4 +24,16 @@ public class Lab1 {
 			System.exit(1);
 		}
 	}
+}
+
+class trainController extends Thread {
+	
+	public trainController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    public void run(){
+        System.out.println("MyThread running");
+     }
+	
 }
